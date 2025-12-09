e3cc84cf333ef7da5b2987fc2cee6788e12d81f5
f <mcieplok@gmail.com>
2025-12-09 09:34:34 +0100

    plotting RC on IRK but with wrong input data

--- demos/irk_results/plot.py
+++ demos/irk_results/plot.py
@@ -1,21 +1,27 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+algorithm = "Gauss2"
+system = "RC"
 
-data = np.loadtxt("data.txt", usecols=range(0, 9),   delimiter=",", skiprows=1)
+data = np.loadtxt(
+    "data.txt" if system == "Mass-Spring" else "RC_data.txt",
+    usecols=range(0, 9),
+    delimiter=",",
+    skiprows=1,
+)
 
-algorithm = "Radau3"
-def resolve_algorithm (name: str):
+
+def resolve_algorithm(name: str):
     if name == "Gauss2":
-        return (1,2)
+        return (1, 2)
     if name == "Gauss3":
-        return (3,4)
+        return (3, 4)
     if name == "Radau2":
-        return (5,6)
+        return (5, 6)
     if name == "Radau3":
-        return (7,8)
+        return (7, 8)
     raise ValueError(f"Unknown algorithm name: {name}")
-
 
 
 time_col = 0
@@ -25,7 +31,7 @@ plt.plot(data[:, time_col], data[:, x_col], label="position")
 plt.plot(data[:, time_col], data[:, y_col], label="velocity")
 plt.xlabel("time")
 plt.ylabel("value")
-plt.title(f"Mass-Spring System Time Evolution {algorithm}")
+plt.title(f"Mass-Spring System Time Evolution {algorithm} {system}")
 plt.legend()
 plt.grid()
 plt.savefig(f"Time evolution {algorithm}.png", dpi=300)
@@ -35,7 +41,7 @@ plt.close()
 plt.plot(data[:, 1], data[:, 2], label="phase plot")
 plt.xlabel("position")
 plt.ylabel("velocity")
-plt.title(f"Mass-Spring System Phase Plot {algorithm}")
+plt.title(f"Mass-Spring System Phase Plot {algorithm} {system}")
 plt.legend()
 plt.grid()
 plt.savefig(f"Result {algorithm}.png", dpi=300)
